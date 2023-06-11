a20add07847f8aa9af564c63ec332249f1fff70d
DenysAbramets <115029520+DenysAbramets@users.noreply.github.com>
2023-06-11 21:02:37 +0000

    Deploying to gh-pages from @ DenysAbramets/goit-react-hw-08-phonebook@0f38c30b956d702cba7a59a66e0ce800fdecf633 🚀

---
+++ static/js/415.c925708c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,n,t){t.r(n);var c,i,r=t(168),a=t(9202),l=t(184),h=a.Z.div(c||(c=(0,r.Z)(["\n  display: flex;\n  align-items: center;\n  justify-content: center;\n  min-height: calc(100vh - 50px);\n"]))),o=a.Z.h1(i||(i=(0,r.Z)(["\n  font-weight: 500;\n  font-size: 48px;\n  text-align: center;\n"])));n.default=function(){return(0,l.jsx)(h,{children:(0,l.jsx)(o,{children:"Welcome to my Heart,BUBA\ud83d\udc95!"})})}}}]);
+//# sourceMappingURL=415.c925708c.chunk.js.map
